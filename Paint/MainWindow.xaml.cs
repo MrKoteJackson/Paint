a47199e89d03910c9354174e7f25726892994f9f
dmatseku <dmatseku@gmail.com>
2021-08-26 23:32:52 +0300

    base pattern

--- Paint/MainWindow.xaml.cs
+++ Paint/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace Paint
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Point currentPoint = new Point();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Drawing_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(Drawing);
+        }
+
+        private void Drawing_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line();
+
+                line.Stroke = SystemColors.WindowFrameBrush;
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition(Drawing).X;
+                line.Y2 = e.GetPosition(Drawing).Y;
+
+                currentPoint = e.GetPosition(Drawing);
+
+                Drawing.Children.Add(line);
+            }
         }
     }
 }
